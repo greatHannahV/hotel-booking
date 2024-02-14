543894077284f362b7b5959701969acb26871c1b
greatHannahV <i.kasl@mail.ru>
2024-02-14 18:43:08 +0200

    created routes to every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,30 @@
-import styled from 'styled-components'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import Settings from './pages/Settings'
+import PageNotFound from './pages/PageNotFound'
 import GlobalStyles from './styles/GlobalStyles'
-import Button from './ui/Button'
-import Input from './ui/Input'
-import Heading from './ui/Heading'
-import Row from './ui/Row'
-
-const StyledApp = styled.div`
-  padding: 20px;
-`
-
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Pine Hotel</Heading>
-            <div>
-              <Heading as="h2">Form</Heading>
-              <Button onClick={() => alert('check in now')}>Check in</Button>
-              <Button
-                variation="secondary"
-                onClick={() => alert('check out now')}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as="h3">Footer</Heading>
-            <form>
-              <Input type="number" placeholder="Input" />
-              <Input type="number" placeholder="Number" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
