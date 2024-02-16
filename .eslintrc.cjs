5bd5c2ba57f79b63898e1c986e66ed6ff116c3aa
greatHannahV <i.kasl@mail.ru>
2024-02-16 22:26:22 +0200

    started to use react-query

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
-    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
