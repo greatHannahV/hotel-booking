7208cf0c73484555884007b677dbbc674972616c
greatHannahV <i.kasl@mail.ru>
2024-02-16 21:31:37 +0200

    queried info from the supabase

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,13 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://tudhjufvdrkmtrvhmqrs.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1ZGhqdWZ2ZHJrbXRydmhtcXJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwOTk1MDcsImV4cCI6MjAyMzY3NTUwN30.o5FOrZju2iH2_ZSf0oUYoodqC8l6VIGsgIwdJmSF5is'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
