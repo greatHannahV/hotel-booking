01bb866059d0e67d93d034e1515f6ca4236abcfa
greatHannahV <i.kasl@mail.ru>
2024-02-23 21:09:58 +0200

    added cabins info

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,8 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
+    'eslint:recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
@@ -13,9 +13,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh', 'react'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
   },
 }
