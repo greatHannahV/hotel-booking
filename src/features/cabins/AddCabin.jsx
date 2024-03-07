1e1440d79c233a9f616243de8d9efe9b1ea7df34
greatHannahV <i.kasl@mail.ru>
2024-03-07 17:59:48 +0200

    compound component pattern was used for the cabin's button

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,27 +1,47 @@
-import { useState } from 'react'
 import CreateCabinForm from './CreateCabinForm'
 import Button from '../../ui/Button'
 import Modal from '../../ui/Modal'
-
+import CabinTable from './CabinTable'
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false)
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add a new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm
-            onCloseModal={() => {
-              setIsOpenModal(false)
-            }}
-          />
-        </Modal>
-      )}
-      {/* {isOpenModal && <CreateCabinForm />} */}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add a new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Open the table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   )
 }
-
 export default AddCabin
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false)
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add a new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm
+//             onCloseModal={() => {
+//               setIsOpenModal(false)
+//             }}
+//           />
+//         </Modal>
+//       )}
+//       {/* {isOpenModal && <CreateCabinForm />} */}
+//     </div>
+//   )
+// }
+
+// export default AddCabin
