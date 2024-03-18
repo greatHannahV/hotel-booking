a2d7ee684355a66616f772fbac08242374fa48ae
greatHannahV <i.kasl@mail.ru>
2024-03-18 19:08:16 +0200

    added a sorf fn

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from '../../ui/TableOperations'
 import Filter from '../../ui/Filter'
+import SortBy from '../../ui/SortBy'
 function CabinTableOperations() {
   return (
     <TableOperations>
@@ -9,6 +10,16 @@ function CabinTableOperations() {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No discount' },
           { value: 'with-discount', label: 'With discount' },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: 'name-asc', label: 'Sort by name (A-Z)' },
+          { value: 'name-disc', label: 'Sort by name (Z-A)' },
+          { value: 'reqularPrice-asc', label: 'Sort by price (low first)' },
+          { value: 'reqularPrice-disc', label: 'Sort by price (high first)' },
+          { value: 'maxCapacity-asc', label: 'Sort by capacity (low first)' },
+          { value: 'maxCapacity-disc', label: 'Sort by capacity (high first)' },
         ]}
       />
     </TableOperations>
