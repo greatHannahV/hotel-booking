181635ce502b609a962193babad9c5b3e195a214
greatHannahV <i.kasl@mail.ru>
2024-03-19 22:17:04 +0200

    made a sorting method for bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,9 +8,13 @@ export async function getBookings({ filter, sortBy }) {
       'id, created_at, startDate, endDate, numNights, numGuests, status,totalPrice, cabins(name), guests(fullName,email)',
     )
 
-  //filter
+  //Filter
 
-  if (filter !== null) query = query.eq(filter.field, filter.value)
+  if (filter) query = query.eq(filter.field, filter.value)
+  //Sort
+  if (sortBy)
+    query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' })
+
   const { data, error } = await query
 
   if (error) {
