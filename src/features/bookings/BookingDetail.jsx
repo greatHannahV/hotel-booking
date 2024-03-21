3f852153abacf8b6eb7f2bee0a3181feb34c9637
greatHannahV <i.kasl@mail.ru>
2024-03-21 18:45:12 +0200

    made a page for a booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,44 +1,45 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import BookingDataBox from "./BookingDataBox";
-import Row from "../../ui/Row";
-import Heading from "../../ui/Heading";
-import Tag from "../../ui/Tag";
-import ButtonGroup from "../../ui/ButtonGroup";
-import Button from "../../ui/Button";
-import ButtonText from "../../ui/ButtonText";
+import BookingDataBox from './BookingDataBox'
+import Row from '../../ui/Row'
+import Heading from '../../ui/Heading'
+import Tag from '../../ui/Tag'
+import ButtonGroup from '../../ui/ButtonGroup'
+import Button from '../../ui/Button'
+import ButtonText from '../../ui/ButtonText'
+import Spinner from '../../ui/Spinner'
 
-import { useMoveBack } from "../../hooks/useMoveBack";
+import { useMoveBack } from '../../hooks/useMoveBack'
+import { useBooking } from './useBooking'
 
 const HeadingGroup = styled.div`
   display: flex;
   gap: 2.4rem;
   align-items: center;
-`;
+`
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
-
-  const moveBack = useMoveBack();
-
+  const { booking, isLoading } = useBooking()
+  const { status, id: bookingId } = booking
+  const moveBack = useMoveBack()
   const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+    unconfirmed: 'blue',
+    'checked-in': 'green',
+    'checked-out': 'silver',
+  }
 
+  if (isLoading) return <Spinner />
   return (
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
+          <Heading as="h1">Booking #{bookingId}</Heading>
+          <Tag type={statusToTagName[status]}>{status.replace('-', ' ')}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
 
-      <BookingDataBox booking={booking} />
+      {/* <BookingDataBox booking={booking} /> */}
 
       <ButtonGroup>
         <Button variation="secondary" onClick={moveBack}>
@@ -46,7 +47,7 @@ function BookingDetail() {
         </Button>
       </ButtonGroup>
     </>
-  );
+  )
 }
 
-export default BookingDetail;
+export default BookingDetail
