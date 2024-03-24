813be393081d52b575438b4861cb6eb506f859d6
greatHannahV <i.kasl@mail.ru>
2024-03-24 22:04:29 +0200

    implemented auth-ion

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,20 @@
+import { useMutation } from '@tanstack/react-query'
+import { login as loginApi } from '../../services/apiAuth'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+
+export function useLogin() {
+  const navigate = useNavigate()
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      toast.success('Successfully loged in')
+      // console.log(user)
+      navigate('/dashboard')
+    },
+    onError: (err) => {
+      toast.error(err.message)
+    },
+  })
+  return { login, isLoading }
+}
