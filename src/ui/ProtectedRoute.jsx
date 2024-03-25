6f757dfc05b4a2a9e7267cb89b6b2efabec4a19a
greatHannahV <i.kasl@mail.ru>
2024-03-25 15:41:33 +0200

    imolemented login only for auth-ed users

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import { useUser } from '../features/authentication/useUser'
+import Spinner from './Spinner'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate()
+  //1.load the auth-ed user
+  const { isLoading, isAuthenticated } = useUser()
+
+  //3.if there is no an auth-ed user-redirect to login page
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate('/login')
+    },
+    [isAuthenticated, isLoading, navigate],
+  )
+  //2.while is loading-show the spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    )
+  //4.if there is a user-render the app
+  if (isAuthenticated) return children
+}
+
+export default ProtectedRoute
