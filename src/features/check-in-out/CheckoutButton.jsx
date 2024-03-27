0f8f66004ac85d5e2f78a1ad229a4772c0cc86d6
greatHannahV <i.kasl@mail.ru>
2024-03-27 22:43:53 +0200

    used react-error- Boundaries

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,17 +1,30 @@
 import Button from '../../ui/Button'
+import ConfirmDelete from '../../ui/ConfirmDelete'
+import Modal from '../../ui/Modal'
 import { useCheckout } from './useCheckout'
 
 function CheckoutButton({ bookingId }) {
   const { checkout, isCheckingOut } = useCheckout()
   return (
-    <Button
-      variation="primary"
-      size="small"
-      onClick={() => checkout(bookingId)}
-      disabled={isCheckingOut}
-    >
-      Check out
-    </Button>
+    <Modal>
+      <Modal.Open opens="delete">
+        <Button
+          variation="primary"
+          size="small"
+          // onClick={() => checkout(bookingId)}
+          // disabled={isCheckingOut}
+        >
+          Check out
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="delete">
+        <ConfirmDelete
+          resourceName="booking"
+          onConfirm={() => checkout(bookingId)}
+          disabled={isCheckingOut}
+        />
+      </Modal.Window>
+    </Modal>
   )
 }
 
